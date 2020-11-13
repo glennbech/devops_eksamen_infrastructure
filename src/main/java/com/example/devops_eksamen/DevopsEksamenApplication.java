523014013557f14c373bdc01b736fed04ea73ddb
bragekankode <KnulleGutta1997>
2020-11-13 13:51:04 +0100

    Initial commit

---
+++ src/main/java/com/example/devops_eksamen/DevopsEksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops_eksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsEksamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsEksamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devops_eksamen/DevopsEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_eksamen;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsEksamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
