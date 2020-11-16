695258bc3da0d1defd9fa4f8ccf543cc970e17d7
bragefridriksson <KnulleGutta1997>
2020-11-16 03:43:08 -0500

    changed bucket again

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "static-site" {
   project = "devopsexam-295612"
-  name = "devopsexaminfrastructurebucket"
+  name = "devopsexaminfrastructurebucket01"
   location = "US"
 }
