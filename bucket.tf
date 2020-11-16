1faff49b41991a9be94e227f6a37d6363e886d1d
bragefridriksson <KnulleGutta1997>
2020-11-16 07:56:15 -0500

    changed image

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "static-site" {
   project = "examinfrastructure-295710"
-  name = "devopsexaminfrastructurebucket"
+  name = "devopsexaminfrastructurebuckettest"
   location = "US"
 }
