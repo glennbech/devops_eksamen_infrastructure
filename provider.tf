94bf95e1455c0979bf7b17ffc9b129befd1cd90f
bragefridriksson <KnulleGutta1997>
2020-11-16 02:54:18 -0500

    Changed provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = "${file("google-key.json")}"
+  credentials = "${file("terraform.json")}"
   project     = "devopsexam-295612"
   region      = "us-central1"
   zone        = "us-central1-c"
