b7b7c7f63fa6a563cef79688efb11ffb95dd9af4
bragekankode <KnulleGutta1997>
2020-11-20 11:24:06 +0100

    added statuscake.tf

---
+++ statuscake.tf
@@ -0,0 +1,12 @@
+provider "statuscake" {
+  username = "braggeliveno"
+  apikey = "STATUSCAKE_APIKEY"
+}
+
+resource "statuscake_test" "googlecloudruntest" {
+  website_name = "My test"
+  website_url  = google_cloud_run_service.default.status[0].url
+  test_type    = "HTTP"
+  check_rate   = 300
+  contact_group   = ["195387"]
+}
