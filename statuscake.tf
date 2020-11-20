6617c291dd2fc361f7876ca5136bc944a7bf0753
bragekankode <KnulleGutta1997>
2020-11-20 12:14:01 +0100

    Trying to fail statuscake

--- statuscake.tf
+++ statuscake.tf
@@ -4,7 +4,7 @@ provider "statuscake" {
 
 resource "statuscake_test" "googlecloudruntest" {
   website_name = "My test"
-  website_url  = google_cloud_run_service.default.status[0].url
+//  website_url  = google_cloud_run_service.default.status[0].url
   test_type    = "HTTP"
   check_rate   = 300
   contact_group   = ["195387"]
